da2a20b5332a271ecb81894778a0eea5ec9854f7
markaeroltomarse <marktomarse@gmail.com>
2021-05-04 02:06:12 +0800

    first commit

---
+++ Scheme/subject.js
@@ -0,0 +1,37 @@
+const Subject = {
+    subCode:{
+        type:String,
+        required:true,
+        description:'ex. MATH424'
+    },
+
+    subName:{
+        type:String,
+        required:true,
+        description:'Subject Name'
+    },
+
+    subDesc:{
+        type:String,
+        required:true,
+        description:'subject description'
+    },
+
+    subCourse:{
+        type:String,
+        required:true,
+        description:'ex. BSIT (from drop down selection)'
+    },
+
+    subUnit:{
+        type:String,
+        required:true,
+        description:'Subject Unit'
+    },
+
+    subUnit:{
+        type:String,
+        required:true,
+        description:'Subject Unit'
+    },
+}
