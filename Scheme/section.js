ed75436de71d78c92f3be754f548e2456f6d3a55
markaeroltomarse <marktomarse@gmail.com>
2021-05-04 21:37:56 +0800

    first commit

---
+++ Scheme/section.js
@@ -0,0 +1,19 @@
+const Section = {
+    secYear:{
+        type:String,
+        required:true,
+        //Section Year
+    },
+
+    secName:{
+        type:String,
+        required:true,
+        //Name of the section
+    },
+
+    course:{
+        type:String,
+        required:true,
+        //Course of the section
+    },
+}
